2f8af32a6195b65eafd2293ef913be2073451b6f
syedubaid <syedabuubaid@gmail.com>
2024-11-02 20:45:57 +0530

    commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,2 +1,15 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server);
+
+
+interface Request extends express.Request {}
+interface Response extends express.Response {}
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Socket IO is running now ");
+});
