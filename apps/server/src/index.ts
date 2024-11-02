6a5299fe5d21bd9a07474923c73307dd9022ea11
syedubaid <syedabuubaid@gmail.com>
2024-11-02 20:55:42 +0530

    commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,9 +7,19 @@ const server=http.createServer(app);
 const io=new Server(server);
 
 
-interface Request extends express.Request {}
-interface Response extends express.Response {}
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: express.Request, res: express.Response): void => {
     res.send("Socket IO is running now ");
-});
+})
+
+io.on('conncetion',(socket)=>{
+    console.log('a user connceted');
+
+    socket.on('disconnect',()=>{
+        console.log('a user disconnected')
+    })
+})
+
+const PORT=process.env.PORT || 3000;
+server.listen(PORT,()=>{
+    console.log(`server is running on PORT ${PORT}`)
+})
